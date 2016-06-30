360d2fbe98dc48c01a68930997d5bbff53838063
Vincent Bernardoff <vb@luminar.eu.org>
2016-06-30 13:08:14 +0200

    poloniex: fix Ws.open_connection

--- src/poloniex_api.ml
+++ src/poloniex_api.ml
@@ -76,30 +76,31 @@ module Ws = struct
     let scheme =
       Option.value_exn ~message:"no scheme in uri" Uri.(scheme uri) in
     let write_wamp w msg = msg |> Wamp.json_of_msg |> Yojson.Safe.to_string |> Pipe.write w in
-    let read_wamp r =
-      Pipe.read r >>| function
-      | `Eof -> raise End_of_file
-      | `Ok response ->
-        let response_json = Yojson.Safe.from_string response in
-        Wamp.msg_of_json response_json
-    in
+    let buf = Bi_outbuf.create 1024 in
+    let read_wamp msg = Yojson.Safe.from_string ~buf msg |> Wamp.msg_of_json in
     let subscribe ~topics r w =
       Deferred.List.map ~how:`Sequential topics ~f:(fun topic ->
           let request_id, subscribe_msg = Wamp.(subscribe ~topic ()) in
           write_wamp w subscribe_msg >>= fun () ->
-          read_wamp r >>| fun msg ->
-          Wamp.subscribed_of_msg ~request_id msg
+          Pipe.read r >>| function
+          | `Eof -> raise End_of_file
+          | `Ok msg -> Wamp.subscribed_of_msg ~request_id @@ read_wamp msg
         )
     in
     let client_r, client_w = Pipe.create () in
     let process_ws r w =
       (* Initialize *)
       write_wamp w Wamp.(hello ~realm:"realm1" ()) >>= fun () ->
-      read_wamp r >>= fun msg -> match msg.Wamp.typ with
-      | Welcome ->
-        subscribe ~topics r w >>= fun _sub_ids ->
-        Pipe.transfer_id r client_w
-      | msgtype -> failwithf "wamp: expected Welcome, got %s" Wamp.(show_msgtype msgtype) ()
+      Pipe.read r >>= function
+      | `Eof -> raise End_of_file
+      | `Ok msg -> begin
+          let msg = read_wamp msg in
+          match msg.Wamp.typ with
+          | Welcome ->
+            subscribe ~topics r w >>= fun _sub_ids ->
+            Pipe.transfer r client_w read_wamp
+          | msgtype -> failwithf "wamp: expected Welcome, got %s" Wamp.(show_msgtype msgtype) ()
+        end
     in
     let tcp_fun s r w =
       Socket.(setopt s Opt.nodelay true);
